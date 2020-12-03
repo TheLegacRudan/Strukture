4a16a21b154997d0623918d490ec79f0b311017d
TheLegacRudan <73548798+TheLegacRudan@users.noreply.github.com>
2020-12-03 17:08:43 +0100

    Add files via upload

---
+++ zdk6.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<time.h>
+
+typedef struct stog *poz;
+typedef struct stog {
+	int x;
+	poz next;
+}st;
+
+int push(int x,poz head){
+	poz q;
+	q = (poz)malloc(sizeof(st));
+
+	q->next = head->next;
+	head->next = q;
+}
+int pop(poz head) {
+	poz q;
+	q = head->next;
+
+	head->next = q->next;
+	free(q);
+}
+int random(){
+	int gg = 100;
+	int dg = 10;
+	int num;
+	num = (rand() %	(gg - dg + 1)) + dg;
+	return num;
+}
+
+
+int main() {
+	st pr1;
+	pr1.next = NULL;
+	srand(time(0));
+	random();
+
+	push(random(), &pr1);
+
+	return 0;
+}
